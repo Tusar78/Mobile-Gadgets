b1577bceea3888c49cea8e4516a7e5c99dda216d
Tusar <tusarahmed141@gmail.com>
2024-07-08 09:50:10 +0600

    Last update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,52 +1,50 @@
+// Neccessary
 const phoneList = document.querySelector(".phones__list");
-// console.log(phoneList);
 
-// Data Get
-const getData = async (searchData) => {
+// Load Data
+const loadData = async (items) => {
   const response = await fetch(
-    `https://openapi.programming-hero.com/api/phones?search=${searchData}`
+    `https://openapi.programming-hero.com/api/phones?search=${items}`
   );
   const data = await response.json();
   const phones = data.data;
   displayData(phones);
 };
 
-// fetch("https://openapi.programming-hero.com/api/phones?search=iphone")
-//   .then((res) => res.json())
-//   .then((data) => displayData(data));
-
 const displayData = (data) => {
-  data.forEach(elem => {
+  phoneList.innerHTML = '';
+  data.forEach((elem) => {
     const card = document.createElement("div");
+    card.classList = `card bg-base-100 w-full shadow-xl`;
     card.innerHTML = `
-        <div class="card bg-base-100 w-full shadow-xl">
-            <figure class="px-10 pt-10">
-                <img
-                src="${elem.image}"
-                alt="Shoes"
-                class="rounded-xl"
-                />
-            </figure>
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions">
-                <button class="btn btn-primary">Buy Now</button>
-                </div>
+        <figure class="px-10 pt-10">
+            <img
+            src="${elem.image}"
+            alt="Shoes"
+            class="rounded-xl"
+            />
+        </figure>
+        <div class="card-body items-center text-center">
+            <h2 class="card-title">${elem.phone_name}</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions">
+            <button class="btn btn-primary">Buy Now</button>
             </div>
         </div>
-    `
+    `;
     phoneList.appendChild(card);
   });
 };
 
-getData('iphone');
+
 
 const getSearch = () => {
   const searchInput = document.querySelector(".search__input ");
   const searchValue = searchInput.value;
-  getData(searchValue);
-}
+  loadData(searchValue);
+};
 
 const searchBtn = document.querySelector(".search__btn");
-searchBtn.addEventListener("click", getSearch)
+searchBtn.addEventListener("click", getSearch);
+
+loadData();
